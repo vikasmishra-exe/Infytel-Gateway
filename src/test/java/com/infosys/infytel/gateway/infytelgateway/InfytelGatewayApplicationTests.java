cbfd9623cffc9799f977f0601c0102fee745d019
Vikas Kumar Mishra <vikaskumar.mishra01@ad.infosys.com>
2024-07-07 09:49:04 +0530

    InfyTel-Gateway Intial Push

---
+++ src/test/java/com/infosys/infytel/gateway/infytelgateway/InfytelGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.infytel.gateway.infytelgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfytelGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
